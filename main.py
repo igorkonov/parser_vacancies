a13e1e7bc357ac2b8c71e6a7f1baba9cb1f5bb33
Bankin93 <igorkonov1993@gmail.com>
2023-03-24 20:05:13 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-import os
-from pprint import pprint
-from engine_classes import HH, SuperJob
-from jobs_classes import *
-import pandas as pd
-import requests
+from jobs_classes import HHVacancy, SJVacancy
+from utils import sorting, get_top, collect_jobs
+
 
 def main():
     input_word = 'python'#input('Введите поисковый запрос:')
-    user_input = "HH"#input("Введите сервис подбора вакансий [HH, SuperJob]:")
-    input_count = 10#input("Сколько вывести вакансий?")
-    input_sorting = 'yes'#input("Вы хотите отсортировать вакансии по зп? yes/no")
-    while True:
-        if user_input == 'HH':
-            engine = HH(user_input)
-            hh_res = engine.get_vacancies_hh()
-            hh_data = HH.get_connector('hh_vacancy.json')
-            hh_data.insert(hh_res)
-            HHVacancy.get_file('hh_vacancy.json')
-            if input_sorting.lower() == 'Yes' or 'yes':
-                get_top(sorting(HHVacancy.hh_vacancies), input_count)
-                print(f'{HHVacancy.get_count_of_vacancy} - количество вакансий от HH')
+    user_input = "SuperJob"#input("Введите сервис подбора вакансий [HH, SuperJob]:")
+    input_count = 2#input("Сколько вывести вакансий?")
+    input_sorting = 'No'#input("Вы хотите отсортировать вакансии по зп? yes/no")
 
+    if user_input.lower() == "hh":
+        vac_1 = collect_jobs(HHVacancy)
+        vac_1.get_vacancies_hh(input_word)
 
+        if input_sorting.lower() == "yes":
+            get_top(sorting(HHVacancy.hh_vacancies), input_count)
+            print(f'{vac_1.get_count_of_vacancy} - количество вакансий от HH')
+        if input_sorting.lower() == "no":
+            get_top(HHVacancy.hh_vacancies, input_count)
+            print(f'{vac_1.get_count_of_vacancy} - количество вакансий от HH')
+
+    if user_input.lower() == "superjob":
+        vac_2 = collect_jobs(SJVacancy)
+        vac_2.get_vacancies_sj(input_word)
+
+        if input_sorting.lower() == "yes":
+            get_top(sorting(SJVacancy.sj_vacancies), input_count)
+            print(f'{vac_2.get_count_of_vacancy} - количество вакансий от SuperJob')
+        if input_sorting.lower() == "no":
+            get_top(SJVacancy.sj_vacancies, input_count)
+            print(f'{vac_2.get_count_of_vacancy} - количество вакансий от SuperJob')
 
 
 if __name__ == '__main__':
